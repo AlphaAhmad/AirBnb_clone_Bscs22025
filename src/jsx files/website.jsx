b5108828fd81b84b2fc7361deb7b72d1f76c8adc
Muhammad Ahmad <muhammadahmad8063@gmail.com>
2024-10-06 15:54:01 +0500

    Search Bar complete

---
+++ src/jsx files/website.jsx
@@ -0,0 +1,13 @@
+import Navbar from './nav.jsx';
+import SearchBar from './search.jsx';
+
+function Website() {
+    return (
+        <>
+            <Navbar />
+            <SearchBar />
+        </>
+    )
+}
+
+export default Website; 
